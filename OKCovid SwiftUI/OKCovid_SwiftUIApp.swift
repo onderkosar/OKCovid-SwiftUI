6a851c7cfa326fe4461fa457fc7684ed7b9468e5
Önder Koşar <onderkosar@hotmail.com>
2020-10-22 18:43:43 +0300

    Initial Commit

---
+++ OKCovid SwiftUI/OKCovid_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  OKCovid_SwiftUIApp.swift
+//  OKCovid SwiftUI
+//
+//  Created by Önder Koşar on 22.10.2020.
+//
+
+import SwiftUI
+
+@main
+struct OKCovid_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
